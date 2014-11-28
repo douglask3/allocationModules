5681a886e6667175b0708238cb2d78c01f8c3da1
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-11-28 11:47:34 +1100

    more tidying

--- MaxW/dimensionless_variables.py
+++ MaxW/dimensionless_variables.py
@@ -5,46 +5,48 @@ __email__   = "douglas.kelley@mq.edu.com"
 
 
 class dimensionless_variables(object):
-	""" Some variables used to in MaxW equations McMurtrie et al. (2013).
-	
-	"""
-	def __init__(self, An, alpha, Kl, I0, N0, nabase, ltot):
-		## From parameter list
-		self.An         = An
-		self.alpha      = alpha
-		self.Kl         = Kl
-		self.I0         = I0
-		self.N0         = N0
-		
-		## Not from paramter list
-		self.zeta       = self.zeta(nabase)
-		self.ExpKLcrit0 = self.ExpKLcrit0(ltot, nabase)
-		self.Lcrit0     = self.Lcrit0(ltot, nabase)
-		
-	
-	def zeta(self, nabase):	
-		return(self.An*nabase / (self.alpha * self.Kl * self.I0))
-	
-	def ExpKLcrit0(self, ltot, nabase):
-	    a = (1.0 - self.N0 / nabase)
-	    b = self.zeta * exp(self.Kl * ltot)
-	    
-	    den = (b + 1.0 / a)** (0.5) - 1.0
-	    num = self.zeta * (1.0 - (self.N0 / nabase))
-	    return(den / num)
-	    
-	def Lcrit0(self,ltot,nabase):
-	    return( (1.0 / self.Kl) * log(self.ExpKLcrit0) )
-	    
-	def Lcrit(self):
-	    return(0.0 if self.Lcrit0  < 0.0 else self.Lcrit0)
-	    
-	def ExpKLcrit(self):
-	    return(1.0 if self.Lcrit0 < 0.0 else self.ExpKLcrit0)
-		
+    """ Some variables used to in MaxW equations McMurtrie et al. (2013).
+    
+    """
+    def __init__(self, An, alpha, Kl, I0, N0, nabase, ltot):
+        ## From parameter list
+        self.An         = An
+        self.alpha      = alpha
+        self.Kl         = Kl
+        self.I0         = I0
+        self.N0         = N0
+        
+        ## Not from paramter list
+        self.zeta       = self.zeta(nabase)
+        self.ExpKlcrit_denominator = self.ExpKlcrit_denominator(ltot,nabase)
+        self.ExpKLcrit0 = self.ExpKLcrit0(ltot, nabase)
+        self.Lcrit0     = self.Lcrit0(ltot, nabase)
+        
+    
+    def zeta(self, nabase):    
+        return(self.An*nabase / (self.alpha * self.Kl * self.I0))
+    
+    def ExpKlcrit_denominator(ltot,nabase):
+         a = (1.0 - self.N0 / nabase)
+         b = self.zeta * exp(self.Kl * ltot)
+         return( (b + 1.0 / a)** (0.5) - 1.0 )
+    
+    def ExpKLcrit0(self, ltot, nabase):
+        num = self.zeta * (1.0 - (self.N0 / nabase))
+        return( self.ExpKlcrit0_denominator / num )
+        
+    def Lcrit0(self,ltot,nabase):
+        return( (1.0 / self.Kl) * log(self.ExpKLcrit0) )
+        
+    def Lcrit(self):
+        return(0.0 if self.Lcrit0  < 0.0 else self.Lcrit0)
+        
+    def ExpKLcrit(self):
+        return(1.0 if self.Lcrit0 < 0.0 else self.ExpKLcrit0)
+        
 """ References
-	==========
-		McMurtrie RE, Dewar RC. New insights into carbon allocation by trees
-	from the hypothesis that annual wood production is maximized. New Phytol.
-	2013;199(4):981-90. doi:10.1111/nph.12344.
+    ==========
+        McMurtrie RE, Dewar RC. New insights into carbon allocation by trees
+    from the hypothesis that annual wood production is maximized. New Phytol.
+    2013;199(4):981-90. doi:10.1111/nph.12344.
 """
--- MaxW/photosythesis.py
+++ MaxW/photosythesis.py
@@ -3,45 +3,44 @@ from math import exp
 __author__  = "Douglas Kelley"
 __email__   = "douglas.kelley@mq.edu.com"
 
-
 class photosythesis(object):
-	""" Photosythesis equations used to drive MaxW in McMurtrie et al. (2013).
-	
-	"""
-	def __init__(self, An, N0, Kl, Rleaf, I0, alpha, convfactor):
-		self.An		= An
-		self.N0		= N0
-		self.Kl		= Kl
-		self.Rleaf	= Rleaf
-		self.I0		= I0
-		self.alpha	= alpha
-		self.convfactor=convfactor
-	
-	def Asat(self, na):	
-		return(self.An * (na - self.N0))
-	
-	def I(self, lai):
-		return(self.Kl * self.I0 * exp(-self.Kl * lai))
-	
-	def Aa(self, LAI, na):
-		a = (1.0 / self.Asat(na))+(1.0 / (self.alpha * self.I(LAI)))
-		a = (1.0 / a)-self.Rleaf * na
-		a = a * self.convfactor
-		
-		return(a)
-		
-	def dAdN(self, lai, na):
-		Asat= self.Asat(na)
-		
-		a = (1.0 + Asat / (1.0 + Asat / (self.alpha * self.I(lai)))) ** 2.0
-		a = self.An / a
-		a = a - self.Rleaf
-		a = a * self.convfactor
-		
-		return(a)
+    """ Photosythesis equations used to drive MaxW in McMurtrie et al. (2013).
+    
+    """
+    def __init__(self, An, N0, Kl, Rleaf, I0, alpha, convfactor):
+        self.An        = An
+        self.N0        = N0
+        self.Kl        = Kl
+        self.Rleaf     = Rleaf
+        self.I0        = I0
+        self.alpha     = alpha
+        self.convfactor=convfactor
+    
+    def Asat(self, na):    
+        return(self.An * (na - self.N0))
+    
+    def I(self, lai):
+        return(self.Kl * self.I0 * exp(-self.Kl * lai))
+    
+    def Aa(self, LAI, na):
+        a = (1.0 / self.Asat(na))+(1.0 / (self.alpha * self.I(LAI)))
+        a = (1.0 / a)-self.Rleaf * na
+        a = a * self.convfactor
+        
+        return(a)
+        
+    def dAdN(self, lai, na):
+        Asat= self.Asat(na)
+        
+        a = (1.0 + Asat / (1.0 + Asat / (self.alpha * self.I(lai)))) ** 2.0
+        a = self.An / a
+        a = a - self.Rleaf
+        a = a * self.convfactor
+        
+        return(a)
 """ References
-	==========
-		McMurtrie RE, Dewar RC. New insights into carbon allocation by trees
-	from the hypothesis that annual wood production is maximized. New Phytol.
-	2013;199(4):981-90. doi:10.1111/nph.12344.
+    ==========
+        McMurtrie RE, Dewar RC. New insights into carbon allocation by trees
+    from the hypothesis that annual wood production is maximized. New Phytol.
+    2013;199(4):981-90. doi:10.1111/nph.12344.
 """
