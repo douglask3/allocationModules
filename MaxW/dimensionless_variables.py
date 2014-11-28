418a4bb540726c7ce0f0165f181447a83d0d26c2
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-11-28 11:14:45 +1100

    class for dimernsionless variables that will be used for the N A etc tots

--- MaxW/dimensionless_variables.py
+++ MaxW/dimensionless_variables.py
@@ -9,8 +9,9 @@ class dimensionless_variables(object):
 	""" Some variables used to in MaxW equations McMurtrie et al. (2013).
 	
 	"""
-	def __init__(self,alpha,Kl,I0,N0,nabase,ltot):
+	def __init__(self,An,alpha,Kl,I0,N0,nabase,ltot):
 		## From parameter list
+		self.An=An
 		self.alpha=alpha
 		self.Kl=Kl
 		self.I0=I0
@@ -23,24 +24,24 @@ class dimensionless_variables(object):
 		
 	
 	def zeta(self,nabase):	
-		return(An*nabase/(alpha*Kl*I0))
+		return(self.An*nabase/(self.alpha*self.Kl*self.I0))
 	
-	def ExpKLcrit0(ltot,nabase):
-	    a=(1-N0/nabase)
-	    b=self.zeta*exp(self.Kl-ltot)
+	def ExpKLcrit0(self,ltot,nabase):
+	    a=(1.0-self.N0/nabase)
+	    b=self.zeta*exp(self.Kl*ltot)
 	    
-	    den=(b+1/a)**(0.5)-1
-	    num=self.zeta*(1-(N0/nabase))
+	    den=(b+1.0/a)**(0.5)-1.0
+	    num=self.zeta*(1.0-(self.N0/nabase))
 	    return(den/num)
 	    
-	def Lcrit0(ltot,nabase):
-	    return((1/self.Kl)*log(self.ExpKLcrit0))
+	def Lcrit0(self,ltot,nabase):
+	    return((1.0/self.Kl)*log(self.ExpKLcrit0))
 	    
-	def Lcrit():
-	    if self.Lcrit0<0: return(0) else: return(self.Lcrit0)
+	def Lcrit(self):
+	    return(0.0 if self.Lcrit0<0.0 else self.Lcrit0)
 	    
-	def ExpKLcrit():
-	    if self.Lcrit0<0: return(1) else: return(self.ExpKLcrit0(ltot,nabase))
+	def ExpKLcrit(self):
+	    return(1.0 if self.Lcrit0<0.0 else self.ExpKLcrit0)
 		
 """ References
 	==========
