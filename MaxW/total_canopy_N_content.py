740fbc9cf29de80b7b8338a40b533890f2d71c7b
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-01 16:54:34 +1100

    corrections

--- MaxW/total_canopy_N_content.py
+++ MaxW/total_canopy_N_content.py
@@ -9,26 +9,26 @@ class total_canopy_N_content(object):
     """ Some variables used to in MaxW equations McMurtrie et al. (2013).
     
     """
-    def __init__(self, An, alpha, Kl, I0, N0, nabase, ltot):
+    def __init__(self, An, alpha, Kl, I0, N0):
 
         self.N0     = N0
         self.alpha  = alpha
+        self.Kl		= Kl
         self.I0     = I0
         self.An     = An
+        self.DV = dimensionless_variables(self.An, self.alpha, self.Kl, self.I0, self.N0)
         
-    def ntot(ltot, nabase):
-        DV = dimensionless_variables(self.An, self.alpha, self.Kl, self.I0, self.N0,
-             nabase, ltot)
+    def ntot(self,ltot, nabase):
              
         a = self.alpha * self.I0 / self.An
-        b = 1.0 - self.N0 / nabase * DV.ExpKlcrit_denominator
-        c = 1.0 - (1.0 /DV.ExpKLcrit(ltot, nabase))
+        b = self.DV.ExpKlcrit_denominator(ltot,nabase)
+        c = 1.0 - (1.0 /self.DV.ExpKLcrit(ltot, nabase))
         d = nabase * ltot
-        e = nabase - self.No
-        f = DV.Lcrit
+        e = nabase - self.N0
+        f =self.DV.Lcrit(ltot,nabase)
         
         return( a *  b * c + d - e * f)
         
-    def ntot2(ltot, nabase):
+    def ntot2(self,ltot, nabase):
         return(ntot(ltot, nabase) if self.DV.Lcrit(ltot, nabse) > 0 else ltot * nabase )
         
