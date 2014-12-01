775e155ff5eaac03ae2b8f6f2dd5045d55073684
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-01 18:29:30 +1100

    starting to code Contributions to canopy photosynthesis

--- MaxW/photosythesis.py
+++ MaxW/photosythesis.py
@@ -1,21 +1,24 @@
 from math import exp
+from dimensionless_variables import dimensionless_variables
 
 __author__  = "Douglas Kelley"
 __email__   = "douglas.kelley@mq.edu.com"
 
 class photosythesis(object):
     """ Photosythesis equations used to drive MaxW in McMurtrie et al. (2013).
-    
     """
     def __init__(self, An, N0, Kl, Rleaf, I0, alpha, convfactor):
-        self.An        = An
-        self.N0        = N0
-        self.Kl        = Kl
-        self.Rleaf     = Rleaf
-        self.I0        = I0
-        self.alpha     = alpha
-        self.convfactor=convfactor
+        self.An			= An
+        self.N0			= N0
+        self.Kl			= Kl
+        self.Rleaf		= Rleaf
+        self.I0			= I0
+        self.alpha		= alpha
+        self.convfactor = convfactor
+        self.DV			= dimensionless_variables(An, alpha, Kl, I0, N0)
+    	self
     
+    # This First section may well be removed when imported to Gday
     def Asat(self, na):    
         return(self.An * (na - self.N0))
     
@@ -38,6 +41,38 @@ class photosythesis(object):
         a = a * self.convfactor
         
         return(a)
+        
+    #This section will probably be used
+    
+    def zetaFun(ltot,nabase):
+    	return( (1+self.DV.zeta(nabase)*exp(self.Kl*ltot))**0.5 )
+    
+    def _atotup1(ltot, nabase):
+    	a=self.alpha*self.I0
+    	b=1+self.DV.zeta(nabase)
+    	
+    	return(a*(1-b/self.zetaFun))
+    	
+    def _atotup2(ltot,nabase):
+    	...
+    
+    def atotup(ltot,nabase):
+    	return(self._atotup1(lto,nabase) if self.N0==1 else self._atotup1(lto,nabase) )
+    
+    def _atotlow1(ltot,nabase):
+    	a=-self.zetaFun+self.zetaFun**2
+    	a=log(a/self.DV.zeta(nabase))
+    	
+    	b=self.An.nabase
+    	
+    	return(b * (ltot-(1/self.Kl)*a))
+    	
+    def _atotlow2(ltot,nabase):
+    	...
+    	
+    def atotlow (ltot,nabase):
+    	return(self._atotlow1(lto,nabase) if self.N0==1 else self._atotlow2(lto,nabase) )
+    		
 """ References
     ==========
         McMurtrie RE, Dewar RC. New insights into carbon allocation by trees
