19824c16c8e8fd99849c69a4c087682f5251d1dd
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-04 10:47:13 +1100

    coding up 'Use formula fon bootoms of canopy and root system to express Dmax as function of ltot. Untested

--- MaxW/root_N_optimization.py
+++ MaxW/root_N_optimization.py
@@ -11,10 +11,12 @@ class root_N_optimization(object):
     """ Calculation of LAI as uses in McMurtrie et al. (2013).
     
     """
-    def __init__(self,D0, R0):
+    def __init__(self,D0, R0, Nw, CUE):
         ## From parameter list
         self.D0		= D0
         self.R0		= R0
+        self.Nw		= Nw
+        self.CUE	= CUE
     	
     	
     def utot(self, dmax, umax):
@@ -39,5 +41,45 @@ class root_N_optimization(object):
     
     def dmax1(rtot,dmaxsoln=1):
     	newton(self._rotOpt, dmaxsoln, args = (nrtot) )
+      
         
-        
+    def DmaxVsLtot(ltot,nabase,nf,nr,Umax1):
+    	a=log((umax,Tauf)/(R0.D0))
+    	
+    	b=(1-self.Retrans)
+    	c=self.CUE.slef.convfactor.self.Tauf
+    	d=-self.RleafAn/self.DV.zetaFun2(ltot,nabase)
+    	e=1/nf
+    
+    	c=c*d-e
+    	
+    	b=b/c
+    	
+    	return(self.D0*(a-log(b+nr)))
+    
+    def C_Nbal(dmax,ltot,nabase,nf,nr,umax):
+    	utot=self.utot(dmax,umax)
+    	atot=self.atot(ltot,nabase)
+    	ntot=self.ntot(ltot,nabase)
+    	rtot=self.rtot(dmax)
+    	
+    	a=self.Nw*CUE*atot
+    	b=ntot/self.Tauf
+    	c=1-self.Retrans-self.Nw/nf
+    	
+    	d=rtot/self.Taur
+    	e=(nr-Nw)
+    	
+    	return(utot-a-b*c-d*e)
+   
+	def C_Nbal1(ltot,nabase,nf,nr,umax):
+ 		DVL=self.DmaxVsLtot(ltot,nabase,nf,nr,umax)
+ 		return(C_Nbal(DVL,ltot,nabase,nf,nr,umax))
+ 		
+ 	def cwmax(dmax,ltot,nabase,nf):
+ 		a=CUE*self.atot(ltot,nabase)
+ 		b=self.ntot(ltot,nabase)/(nf*self.Tauf)
+ 		c=self.rtot(dmax)/self.Taur
+ 		
+ 		return(a - b - c)
+ 	
