1489db49b2deba60f5005b8df5e8d29493ee997b
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-11 09:20:15 +1100

    adding git rev info and adtional information as watermark

---
+++ plots/libs/addGitRev2plot.r
@@ -0,0 +1,15 @@
+addGitRev2plot <- function(additional=NULL) {
+
+    revNo=returnGitVersionNumber()
+    revURL=returnGitRemoteURL()
+    
+    mtestStandard <- function(x,...) mtext(x,side=1,adj=1,cex=0.33,col="#00000077",...)
+    mtestStandard(paste(revURL,revNo,sep=": "),line=-1)
+    
+    if (!is.null(additional)) mtestStandard(additional)
+}
+
+addGitRev2plot.dev.off <- function() {
+    addGitRev2plot()
+    dev.off()
+}
