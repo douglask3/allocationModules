0e6af0a25902abccd9344823f9157cacefad4c18
Douglas Kelley <douglas.kelley@mq.edu.au>
2014-12-10 21:03:28 +1100

    starting some basic cfg stuff for plotting varaibles: Loads git package, sets up project structure, installs libraries and sets some varible info (name, units, how they will be constructed

---
+++ plots/cfg.r
@@ -0,0 +1,27 @@
+##########################################################################################
+## Seup project and load packages                                                       ##
+##########################################################################################
+library("gitProjectExtras")
+setupProjectStructure()
+sourceAllLibs()
+
+#library("libs/install_and_source_library.r")
+
+
+##########################################################################################
+## Variable Information                                                                 ##
+##########################################################################################
+## 
+VarTitleInfo = rbind(
+                #Name               #Units      #ID
+    leadAl = c("Leaf Allocation"    , "%"),
+    woodAl = c("Wood Allocation"    , "%"),
+    rootAl = c("Root Allocation"    , "%"))
+    
+ 
+VarConstruction = list(
+    leafAl = c(100 , match.fun("*"), "GL", match.fun("/"), "NPP"),
+    woodAl = c(100 , match.fun("*"), "GW", match.fun("/"), "NPP"),
+    rootAl = c(100 , match.fun("*"), "GR", match.fun("/"), "NPP"))
+
+#PlottingInformat =
